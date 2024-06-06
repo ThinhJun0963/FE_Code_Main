f41828d190ae1dc7cfb0a86bb061b4ead30d8b0e
SpaceStalinn <125347214+SpaceStalinn@users.noreply.github.com>
2024-06-06 20:28:38 +0700

    Sửa trang đặt lịch

--- src/pages/Booking/components/CheckoutForm/CheckoutForm.tsx
+++ src/pages/Booking/components/CheckoutForm/CheckoutForm.tsx
@@ -1,64 +1,104 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Divider, Grid, TextField, Typography } from '@mui/material'
-import React from 'react'
-import FolderSharedIcon from '@mui/icons-material/FolderShared';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
+const VNPayFields = ({ amount, handleAmountChange, orderID }: { amount: string, handleAmountChange: (event: React.ChangeEvent<HTMLInputElement>) => void, orderID: string }) => (
+    <>
+        <TextField
+            fullWidth
+            id="amount"
+            label="Giá trị"
+            value={amount}
+            onChange={handleAmountChange}
+            InputLabelProps={{
+                shrink: true,
+            }}
+            sx={{ marginBottom: '20px', marginTop: '20px' }}
+        />
+        <TextField
+            fullWidth
+            id="orderID"
+            label="Mã đơn hàng"
+            value={orderID}
+            InputProps={{
+                readOnly: true,
+            }}
+            InputLabelProps={{
+                shrink: true,
+            }}
+            sx={{ marginBottom: '20px' }}
+        />
+        <TextField
+            fullWidth
+            id="orderInfo"
+            label="Thông tin đơn hàng"
+            value={`Thanh toán ${orderID}`}
+            InputProps={{
+                readOnly: true,
+            }}
+            InputLabelProps={{
+                shrink: true,
+            }}
+        />
+    </>
+);
 
 const CheckoutForm = () => {
+    const [paymentMethod, setPaymentMethod] = useState<string>('');
+    const [amount, setAmount] = useState<string>('');
+    const [orderID, setOrderID] = useState<string>('');
+
+    const handlePaymentMethodChange = (event: SelectChangeEvent<string>) => {
+        const { value } = event.target;
+        setPaymentMethod(value);
+        if (value === 'VNPay') {
+            const newOrderID = Math.floor(Math.random() * 1000000).toString();
+            setOrderID(newOrderID);
+        } else {
+            setOrderID('');
+        }
+    };
+
+    const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAmount(event.target.value);
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        // Perform validation and submission logic here
+        console.log('Form submitted', { paymentMethod, amount, orderID });
+    };
+
     return (
-        <Box component='form' sx={{ marginLeft: '1em', marginRight: '1em' }}>
-            <Typography variant='h4' sx={{ marginBottom: '20px' }}>Thanh toán</Typography>
+        <Box component='form' sx={{ marginLeft: '1em', marginRight: '1em' }} onSubmit={handleSubmit}>
+            <Typography variant='h4' sx={{ marginBottom: '20px' }}>Chọn hình thức thanh toán</Typography>
 
             <Box sx={{ width: '100%' }}>
                 <Grid container spacing={5}>
-                    <Grid item lg={12} >
-                        <Accordion sx={{ width: '100%', backgroundColor: '' }}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1-content"
-                                id="panel1-header"
-                                sx={{ width: '100%' }}
+                    <Grid item lg={12}>
+                        <FormControl fullWidth variant="outlined">
+                            <InputLabel id="payment-method-label">Hình thức thanh toán</InputLabel>
+                            <Select
+                                labelId="payment-method-label"
+                                id="paymentMethod"
+                                value={paymentMethod}
+                                onChange={handlePaymentMethodChange}
+                                label="Hình thức thanh toán"
                             >
-                                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                    <FolderSharedIcon sx={{ height: 'auto', fontSize: '4.5em', color: ' #1976d3' }} />
-                                    <Box >
-                                        <Typography variant='h6'>Nguyễn Văn A</Typography>
-                                    </Box>
-                                </Box>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Box sx={{ }}>
-                                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                        <Typography variant='body1'>Mã bênh nhân</Typography>
-                                        <Typography variant='body2'>BN123456</Typography>
-                                    </Box>
-
-                                    <Divider sx={{ backgroundColor: 'black', width: '100%', margin: '1em auto' }} />
-
-                                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                        <Typography variant='body1'>Họ và tên</Typography>
-                                        <Typography variant='body2'>Nguyễn Văn A</Typography>
-                                    </Box>
-
-                                    <Divider sx={{ backgroundColor: 'black', width: '100%', margin: '1em auto' }} />
-
-                                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                        <Typography variant='body1'>Ngày sinh</Typography>
-                                        <Typography variant='body2'>25/01/2003</Typography>
-                                    </Box>
-
-                                    <Divider sx={{ backgroundColor: 'black', width: '100%', margin: '1em auto' }} />
-
-                                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                        <Typography variant='body1'>Số điện thoại</Typography>
-                                        <Typography variant='body2'>0948290964</Typography>
-                                    </Box>
-                                </Box>
-                            </AccordionDetails>
-                        </Accordion>
+                                <MenuItem value={'VNPay'}>VN Pay</MenuItem>
+                                <MenuItem value={'Other'}>Hình thức khác</MenuItem>
+                            </Select>
+                        </FormControl>
+                        {paymentMethod === 'VNPay' && (
+                            <VNPayFields
+                                amount={amount}
+                                handleAmountChange={handleAmountChange}
+                                orderID={orderID}
+                            />
+                        )}
                     </Grid>
                 </Grid>
             </Box>
+
         </Box>
     )
 }
--- src/pages/Booking/components/TimeSlots/TimeSlots.tsx
+++ src/pages/Booking/components/TimeSlots/TimeSlots.tsx
@@ -1,7 +1,22 @@
 import { Box, Button, Divider, Typography } from '@mui/material';
 import slots from './data';
+import { SetStateAction, useState } from 'react';
 
-const TimeSlots = () => {
+interface TimeSlotsFormProps {
+    setFormData: (value: SetStateAction<{ typeOfBooking: string; date: string; time: string; }>) => void;
+}
+
+const TimeSlots = ({ setFormData }: TimeSlotsFormProps) => {
+    const [slot, setSlot] = useState<string | null>(null);
+
+    const handleSlotClick = (time: string) => {
+        setSlot(time);
+        setFormData(prevState => ({
+            ...prevState,
+            time: time
+        }));
+    }
+
     const morningSlots = slots.filter(slot => {
         const startTime = slot.split('-')[0];
         const [hour] = startTime.split(':');
@@ -20,7 +35,10 @@ const TimeSlots = () => {
                 <Typography variant="h6">Buổi sáng</Typography>
                 <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 1 }}>
                     {morningSlots.map((time, index) => (
-                        <Button key={index} variant="contained">
+                        <Button key={index}
+                            variant={slot === time ? 'contained' : 'outlined'}
+                            onClick={() => handleSlotClick(time)}
+                        >
                             {time}
                         </Button>
                     ))}
@@ -32,7 +50,10 @@ const TimeSlots = () => {
                 <Typography variant="h6">Buổi chiều</Typography>
                 <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 1 }}>
                     {afternoonSlots.map((time, index) => (
-                        <Button key={index} variant="contained">
+                        <Button key={index}
+                            variant={slot === time ? 'contained' : 'outlined'}
+                            onClick={() => handleSlotClick(time)}
+                        >
                             {time}
                         </Button>
                     ))}
