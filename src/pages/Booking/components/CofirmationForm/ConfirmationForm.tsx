0acb775003ab84fda60287a002828147fecdfd80
SpaceStalinn <125347214+SpaceStalinn@users.noreply.github.com>
2024-06-09 16:57:20 +0700

    Sửa confirmation form

--- src/pages/Booking/components/CofirmationForm/ConfirmationForm.tsx
+++ src/pages/Booking/components/CofirmationForm/ConfirmationForm.tsx
@@ -1,50 +1,74 @@
-import { Box, Grid, TextField, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Button, Grid, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
-const ConfirmationForm = () => {
+interface FormData {
+    typeOfBooking: string;
+    date: string;
+    time: string;
+  }
+  
+  interface ConfirmationFormProps {
+    formData: FormData;
+    setFormData: React.Dispatch<React.SetStateAction<FormData>>;
+  }
+  
+const ConfirmationForm = ({ formData, setFormData }: ConfirmationFormProps) => {
+    const [isClicked, setIsClicked] = useState(false)
+  
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    };
+  
     return (
-        <Box component='form' sx={{marginLeft: '10px', maxWidth: '600px' }}>
+        <Box component='form' sx={{ marginLeft: '10px', marginRight: '10px', padding: '10px' }}>
             <Typography variant='h4' sx={{ marginBottom: '20px' }}>Xác nhận thông tin</Typography>
-            <Box>
-                <Grid container spacing={5}>
-                    
-                    <Grid item lg={12}>
-                        <TextField
-                            fullWidth
-                            id="date"
-                            label="Ngày khám"
-                            name="date"
-                            placeholder='20/08/2024'
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-                    </Grid>
-                    <Grid item lg={12}>
-                        <TextField
-                            fullWidth
-                            id="time"
-                            label="Giờ khám"
-                            name="time"
-                            placeholder='14:00 - 14:30'
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-                    </Grid>
-                    <Grid item lg={12}>
-                        <TextField
-                            fullWidth
-                            id="service"
-                            label="Dịch vu *nếu có"
-                            name="service"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-                    </Grid>
+            <Grid container spacing={5}>
+  
+                <Grid item lg={12}>
+                    <TextField
+                        fullWidth
+                        id="date"
+                        label="Ngày khám"
+                        name="date"
+                        placeholder='20/08/2024'
+                        value={formData.date}
+                        onChange={handleChange}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
                 </Grid>
-            </Box>
+                <Grid item lg={12}>
+                    <TextField
+                        fullWidth
+                        id="time"
+                        label="Giờ khám"
+                        name="time"
+                        placeholder='14:00 - 14:30'
+                        value={formData.time}
+                        onChange={handleChange}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </Grid>
+                <Grid item lg={12}>
+                    <TextField
+                        fullWidth
+                        id="service"
+                        label="Dịch vu *nếu có"
+                        name="typeOfBooking"
+                        value={formData.typeOfBooking}
+                        onChange={handleChange}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </Grid>
+            </Grid>
         </Box>
     )
 }
